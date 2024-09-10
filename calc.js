b515f5b405ab8b8e6dc7c49a4da8f446a26c82f9
Israel Arenas <israel.djaz@gmail.com>
2024-09-10 14:10:40 +0200

    metodos y funciones en el dom

--- calc.js
+++ calc.js
@@ -1,31 +1,89 @@
 document.addEventListener("DOMContentLoaded", () => {
   const numeros = document.getElementsByName("number");
   const operadores = document.getElementsByName("operador");
-  const resultado_btn = document.getElementsByName("btn_result");
-  const limpiar = document.getElementsByName("btn_limpiar");
+  const igual = document.getElementsByName("btn_result")[0];
+  const borrar = document.getElementsByName("btn_limpiar")[0];
+  var resultado = document.getElementById("result");
 
-  let resultado = document.getElementById("result");
+  var opeActual = "";
+  var opeAnterior = "";
+  var operacion = undefined;
 
   numeros.forEach(function (btn) {
     btn.addEventListener("click", function () {
-      //agregarNumer(btn.innerText);
-      alert(btn.innerText);
+      agregarnumero(btn.innerText);
+      //alert(btn.innerText);
     });
   });
 
   operadores.forEach(function (btn) {
     btn.addEventListener("click", function () {
-      //selecionaroperacion(btn.innerText);
-      alert(btn.innerText);
+      //alert(btn.innerText);
+      selecionaroperacion(btn.innerText);
     });
   });
 
-  resultado_btn.addEventListener("click", function () {
+  igual.addEventListener("click", function () {
     calcular();
     actualizardisplay();
   });
-  limpiar.addEventListener("click", function () {
+
+  borrar.addEventListener("click", function () {
     clear();
     actualizardisplay();
   });
+
+  function selecionaroperacion(op) {
+    if (opeActual === "") return;
+    if (opeAnterior !== "") {
+      calcular();
+    }
+    operacion = op.toString();
+    opeAnterior = opeActual;
+    opeActual = " ";
+  }
+
+  function calcular() {
+    var calculo;
+    const anterior = Number(opeAnterior);
+    const actual = Number(opeActual);
+    if (isNaN(opeAnterior) || isNaN(actual)) return;
+
+    switch (operacion) {
+      case "+":
+        calculo = anterior + actual;
+        break;
+      case "-":
+        calculo = anterior - actual;
+        break;
+      case "/":
+        calculo = anterior / actual;
+        break;
+      case "x":
+        calculo = anterior * actual;
+        break;
+      default:
+        return;
+    }
+    opeActual = calculo;
+    operacion = undefined;
+    opeAnterior = "";
+  }
+
+  function clear() {
+    opeActual = " ";
+    opeAnterior = " ";
+    operacion = undefined;
+  }
+
+  function agregarnumero(num) {
+    opeActual = opeActual.toString() + num.toString();
+    actualizardisplay();
+  }
+
+  function actualizardisplay() {
+    result.value = opeActual;
+  }
+
+  clear();
 });
